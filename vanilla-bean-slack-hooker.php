ca4a88c82c91b015a128c15b1065b8dfcc1c2bc4
Mark Pottie <mark@pottie.com>
2023-05-12 12:43:45 +1000

    bumping version to 5.2.8

--- vanilla-bean-slack-hooker.php
+++ vanilla-bean-slack-hooker.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Vanilla Bean Slack Hooker
  * Plugin URI:        https://www.velvary.com.au
  * Description:       Integrate webhooks into your site for notifications via Slack, Mattermost or others
- * Version:        5.2.7
+ * Version:        5.2.8
  * Author:            Velvary
  * Author URI:        https://www.velvary.com.au
  * License:           GPL-2.0+
@@ -31,7 +31,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 
-const SLACKHOOKER_VERSION = '5.2.7';
+const SLACKHOOKER_VERSION = '5.2.8';
 const SLACKHOOKER_LOGO = 'https://iili.io/F1uU8P.png';
 const SLACKHOOKER_LINK = 'https://www.velvary.com.au/wordpress_plugins/slackhooker';
 const SLACKHOOKER_FOOTERTEXT = 'Slackhooker by Velvary';
